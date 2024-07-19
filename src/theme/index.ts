c76667ebac3bb6452c03fcd2f8976eb72def99ce
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-19 11:00:44 -0300

    feat: add theme config

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,21 @@
 export const theme = {
-  colors: {},
-  font_size: {},
-  font_weight: {},
+  colors: {
+    indigo900: '#14121F',
+    indigo800: '#1B1525',
+    indigo700: '#291F43',
+    indigo600: '#6E56CF',
+    indigo100: '#BAA7FF',
+    indigo50: '#E2DDFE',
+  },
+  font_size: {
+    lg: 16,
+    sm: 14,
+    md: 12,
+  },
+  font_weight: {
+    regular: 'Inter_400Regular',
+    medium: 'Inter_500Medium',
+    semibold: 'Inter_600SemiBold',
+    bold: 'Inter_700Bold',
+  },
 };
