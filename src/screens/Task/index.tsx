ac8d58e8954e3c2bc74c703056a06930cfd9c447
@dev.arthur <contato.arthurreis222@gmail.com>
2024-07-21 11:51:02 -0300

    feat: add inputs date

--- src/screens/Task/index.tsx
+++ src/screens/Task/index.tsx
@@ -1,11 +1,41 @@
-import { SafeAreaView, View } from 'react-native';
-import React from 'react';
-
+import { Pressable, SafeAreaView } from 'react-native';
 import { styles } from './style';
 import TypeBar from '../../components/TypeBar';
 import { Input, Textarea } from '../../components/Input';
 
+import React, { useState } from 'react';
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from '@react-native-community/datetimepicker';
+import { format, isPast } from 'date-fns';
+
 const Task = () => {
+  const [date, setDate] = useState(new Date());
+  const [time, setTime] = useState(new Date());
+
+  const [showDate, setShowDate] = useState(false);
+  const [showTime, setShowTime] = useState(false);
+
+  const handleSelectDate = (e: DateTimePickerEvent, selectData?: Date) => {
+    if (isPast(new Date(selectData || date))) {
+      alert('Defina uma data no futuro');
+      setShowDate(false);
+      return;
+    }
+    setDate(selectData || date);
+    setShowDate(false);
+  };
+
+  const handleSelectTime = (e: DateTimePickerEvent, selectTime?: Date) => {
+    if (isPast(new Date(selectTime || time))) {
+      alert('Defina uma hora no futuro');
+      setShowTime(false);
+      return;
+    }
+    setTime(selectTime || time);
+    setShowTime(false);
+  };
+
   return (
     <SafeAreaView style={styles.taskContainer}>
       <TypeBar />
@@ -16,6 +46,36 @@ const Task = () => {
         multiline
         numberOfLines={4}
       />
+
+      <Pressable onPress={() => setShowDate(true)}>
+        <Input
+          label="Data"
+          editable={false}
+          value={format(date, 'yyyy-MM-dd')}
+        />
+      </Pressable>
+
+      {showDate && (
+        <DateTimePicker
+          value={date}
+          onChange={handleSelectDate}
+          mode="date"
+          display="spinner"
+        />
+      )}
+
+      <Pressable onPress={() => setShowTime(true)}>
+        <Input label="Hora" editable={false} value={format(time, 'HH:mm')} />
+      </Pressable>
+
+      {showTime && (
+        <DateTimePicker
+          value={time}
+          onChange={handleSelectTime}
+          mode="time"
+          display="clock"
+        />
+      )}
     </SafeAreaView>
   );
 };
